a83fbe154918f8e32bc8da11a30140ec1dffef41
Esko Oramaa <esko.oramaa@solita.fi>
2020-12-30 10:42:14 +0200

    Add ros namespace to micrortps_agent

--- fog-drone/run-ros2.sh
+++ fog-drone/run-ros2.sh
@@ -2,10 +2,10 @@
 
 source /opt/ros/foxy/setup.bash
 echo "Start Mavlink Router"
-mavlink-routerd >/fog-drone/mav_router_out.log 2>/fog-drone/mav_router_err.log &
+mavlink-routerd >/fog-drone/mav_routerd_out.log 2>/fog-drone/mav_routerd_err.log &
 echo "Start Mavlink control"
 ros2 launch px4_mavlink_ctrl mavlink_ctrl.launch >/fog-drone/mav_ctrl_out.log 2>/fog-drone/mav_ctrl_err.log &
 echo "Start Micrortps_agent"
-micrortps_agent -t UDP >/fog-drone/urtps_out.log 2>/fog-drone/urtps_err.log &
+micrortps_agent -t UDP -n "$DRONE_DEVICE_ID" >/fog-drone/urtps_out.log 2>/fog-drone/urtps_err.log &
 echo "Start Communication link"
 communication_link -device_id "$DRONE_DEVICE_ID" -mqtt_broker "$MQTT_BROKER_ADDRESS"
